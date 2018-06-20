1c73c811d380cab9e4ee3ef9eae236e4a804e8a5
Anthony Oyathelemhi <tonero91@gmail.com>
2018-06-20 14:13:21 +0000

    create user Schema/Model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+// var passportLocalMongoose = require('passport-local-mongoose');
+
+var UserSchema = new mongoose.Schema({
+    username: { type:String, lowercase: true },
+	email: { type:String, lowercase: true },
+	password: String,
+	firstname: { type:String, lowercase: true },
+	lastname: { type:String, lowercase: true },
+	middlename: { type:String, lowercase: true },
+	image: { type:String, lowercase: true },
+	gender: { type:String, lowercase: true },
+	relationshipStatus: { type:String, lowercase: true },
+	dateOfBirth: Date,
+	phone: String,
+	phone2: String,
+	address: String,
+	address2: String,
+	state: { type:String, lowercase: true },
+	city: { type:String, lowercase: true },
+	localGovernmentOfOrigin: { type:String, lowercase: true },
+	isAdmin: { type: Boolean, default: false},
+	isExecutive: { type: Boolean, default: false},
+	currentPosition: { type: String, lowercase:true, default: 'member'},
+	dateJoined: { type: Date, default: Date.now }
+})
+
+// UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
