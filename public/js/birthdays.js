37f9f92c61e9c863b79ef73198286a5e6052574b
Anthony Oyathelemhi <tonero91@gmail.com>
2018-08-22 00:23:42 +0000

    add loader icon to show when data is being fetched

--- public/js/birthdays.js
+++ public/js/birthdays.js
@@ -1,10 +1,10 @@
 /* global $ */
 
 $('select').on('change', (event) => {
+    $(".loader").removeClass("disabled").addClass("active");
     let month = $('select').val();
     $.getJSON('/apis/birthdays/' + month)
     .then(data => {
-        console.log(data);
         data.sort((a,b) => a.firstname - b.firstname);
         $('tbody').empty();
         data.forEach((elem, index, array) => {
@@ -12,5 +12,7 @@ $('select').on('change', (event) => {
                 `<tr><td>${elem.firstname} ${elem.lastname}</td><td>${elem.gender}</td><td>${elem.dateOfBirth.substring(8,10)}</td><td>${elem.phone}</td><td>${elem.email}</td></tr>`
             )
         })
+        $(".loader").removeClass("active").addClass("disabled");
+        
     })
 })
