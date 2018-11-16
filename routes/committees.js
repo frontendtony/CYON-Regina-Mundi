b0861d6419009a65240aa5706bd3b1d18a2139d8
Anthony Oyathelemhi <tonero91@gmail.com>
2018-11-16 17:02:18 +0100

    create template for adding a new committee and include the route

--- routes/committees.js
+++ routes/committees.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const middleware = require('../middleware');
 const User = require('../models/user');
 
-router.get('/committees', middleware.isLoggedIn, (req, res) => {
+router.get('/committees', (req, res) => {
   res.render('committees', {title: 'CYON | Committees'});
+});
+
+router.get('/committees/new', (req, res) => {
+  res.render('newCommittee', {title: 'CYON | Add new committee'});
 });
 
 module.exports = router;
