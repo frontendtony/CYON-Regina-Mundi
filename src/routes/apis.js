6f630a98a47ffd897a47426081618f560b7f3423
Anthony Oyathelemhi <tonero91@gmail.com>
2018-11-23 02:02:02 +0100

    create endpoints to confirm and make user an admin

--- src/routes/apis.js
+++ src/routes/apis.js
@@ -4,6 +4,15 @@ import User from '../models/user';
 
 const router = express.Router();
 
+const updateById = async (id, data) => {
+  try {
+    await User.findByIdAndUpdate(id, data);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
 router.get('/apis/birthdays/:month', async (req, res) => {
   try {
     const members = await User.find(
@@ -34,6 +43,26 @@ router.get('/apis/updatePosition/:id/:position', async (req, res) => {
       message: 'Could not fetch data, please try again later',
     });
   }
+});
+
+router.put('/api/confirmuser/:id', async (req, res) => {
+  if (updateById(req.params.id, { verified: true })) {
+    return res.status(200).json({ status: true });
+  }
+  return res.status(500).json({
+    status: false,
+    message: 'Could not update profile, contact the system admin',
+  });
+});
+
+router.put('/api/setadmin/:id', async (req, res) => {
+  if (updateById(req.params.id, { isAdmin: true, verified: true })) {
+    return res.json({ status: true });
+  }
+  return res.json({
+    status: false,
+    message: 'Could not update profile, contact the system admin',
+  });
 });
 
 export default router;
